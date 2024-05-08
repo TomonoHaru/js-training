81eb572d6121efb064fa3ef2c18b61b791d117a3
TomonoHaru <chhr22031@g.nihon-u.ac.jp>
2024-05-09 02:49:22 +0900

    時間直すのムズイ

--- src/pages/Lesson07/index.js
+++ src/pages/Lesson07/index.js
@@ -4,7 +4,39 @@ import Chart from "../../components/Chart07";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  for (const item of input) {
+    const d = new Date(`${item.createdAt} UTC`);
+    const year = d.getFullYear();
+    const month = `${d.getMonth() + 1}`.padStart(2, "0");
+    const date = `${d.getDate()}`.padStart(2, "0");
+    item.createdAt = `${year}-${month}-${date}`;
+  }
+  const twitter = Array.from(new Set(input.map(({ createdAt }) => createdAt)));
+  twitter.sort();
+  const count = { tweet: {}, retweet: {} };
+  for (const d of twitter) {
+    count.tweet[d] = 0;
+    count.retweet[d] = 0;
+  };
+  for (const { createdAt, isRetweet } of input) {
+    if (isRetweet) {
+      count.retweet[createdAt] += 1;
+    } else {
+      count.tweet[createdAt] += 1;
+    }
+  }
+  return ["tweet", "retweet"].map((key) => {
+    return {
+      id: key,
+      data: twitter.map((d) => {
+        return {
+          x: d,
+          y: count[key][d],
+        };
+      }),
+    };
+  });
+  // ここを作りましょう！
 };
 
 const Lesson = () => {
